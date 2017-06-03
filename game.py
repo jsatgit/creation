55532d674652b98d4e5bcb95a47ba2c3a7a4b706
James Shi <jshi@live.ca>
2017-06-03 14:43:38 -0700

    click poc

--- game.py
+++ game.py
@@ -11,6 +11,7 @@ class BaseGame(object):
     def start(self):
         pygame.init()
         self.screen = pygame.display.set_mode(self.dim)
+        self.first_render()
         is_running = True
         while is_running:
             self.clock.tick(self.fps)
@@ -27,12 +28,47 @@ def rand_color():
     b = random.randint(0, 255)
     return (r, g, b)
 
+class Resource(object):
+    def __init__(self, name):
+        self.name = name 
+        self.color = rand_color()
+
+class Grid(object):
+    def __init__(self, n, m):
+        self.grid = None
+        self.resources = [
+            Resource('Sigma'),
+            Resource('Doorstep'),
+            Resource('Minger'),
+            Resource('Rainier'),
+            Resource('Realsting'),
+            Resource('Lolcat')
+        ]
+        self.n = n
+        self.m = m
+        self.init_grid()
+
+    def rand_resource(self):
+        i = random.randint(0, len(self.resources) - 1)
+        return self.resources[i]
+
+    def init_grid(self):
+        self.grid = [
+            [self.rand_resource() for j in xrange(self.m)] 
+            for i in xrange(self.n)
+        ]
+
+    def get(self, i, j):
+        return self.grid[i][j]
+
 class Game(BaseGame):
     def __init__(self):
         super(Game, self).__init__()
         self.block_size = 10
+        m, n = self.dim
+        self.grid = Grid(n, m)
 
-    def render(self):
+    def draw_grid(self):
         width, height = self.dim
         n_blocks_wide = width / self.block_size 
         n_blocks_tall = height / self.block_size 
@@ -45,7 +81,21 @@ class Game(BaseGame):
                     self.block_size, 
                     self.block_size
                 ]
-                pygame.draw.rect(self.screen, rand_color(), rect)
+                resource = self.grid.get(i, j)
+                pygame.draw.rect(self.screen, resource.color, rect)
+
+    def first_render(self):
+        self.draw_grid()
+
+    def render(self):
+        left, mid, right = pygame.mouse.get_pressed()
+        if left:
+            w, h = pygame.mouse.get_pos()
+            j = w / self.block_size
+            i = h / self.block_size
+            resource = self.grid.get(i, j)
+            print resource.name
+
 
 game = Game()
 game.start()
